21435f1fce1ecd2e30292f648c896775008b62d4
Satyasai <satyasai1997@gmail.com>
2025-05-22 15:22:07 +0530

    first-commit

---
+++ add.py
@@ -0,0 +1,12 @@
+a =  int(input("Enter the first number:"))
+b = int(input("Enter the second number:"))
+
+addition =  a + b
+subraction =  a - b
+multiplication = a*b
+divison = a/b
+
+print("addition: ", addition)
+print("subraction: ", subraction)
+print("multiplication: ", multiplication)
+print("division: ", divison)
---
+++ name.py
@@ -0,0 +1,4 @@
+firstname =  str(input("Enter your first name: "))
+lastname = str(input("Enter your last lane: "))
+
+print("Hello", firstname + lastname," ! Welcome to the python program.")
